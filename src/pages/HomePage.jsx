75cdaf3937ed5b117999e2e052487989520e8117
Teo Wen Kai <teowenkai96@gmail.com>
2026-01-06 21:37:21 +0800

    changed images name to small i

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ export default function HomePage() {
                     My Projects &rarr;
                 </Link>
             </div>            
-            <img src={`${import.meta.env.BASE_URL}/public/Images/photo_WK.jpeg`} className="twk-photo" alt="Teo Wen Kai's Photo" height="300" width="300"/>
+            <img src={`${import.meta.env.BASE_URL}/public/images/photo_WK.jpeg`} className="twk-photo" alt="Teo Wen Kai's Photo" height="300" width="300"/>
         </div>
     );
 };
