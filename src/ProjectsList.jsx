578295472ee4ce4eaac75cca8bb43712750cd512
Teo Wen Kai <wenkai04@hotmail.com>
2026-09-24 13:59:53 +0800

    Changed <a> back to <Link>

--- src/ProjectsList.jsx
+++ src/ProjectsList.jsx
@@ -4,11 +4,11 @@ export default function ProjectsList({articles}) {
             <div>
                 {articles.map(a => (
                     <div className="pb-20" key={a.name}>
-                        <a key={a.name} href={"/portfolio/projects/" + a.name}>
+                        <Link key={a.name} to={"/portfolio/projects/" + a.name}>
                             {a.image && <img src={a.image} alt={a.title} className="aspect-[18/9] md:h-[60vh] object-cover object-top"/>}
                             <h3 className="py-5 text-xl font-medium">{a.title}</h3>
                             <p className="text-[#6D75BB] hover:text-gray-500">Learn More &rarr;</p>
-                        </a>
+                        </Link>
                     </div>
                 ))}
             </div>
