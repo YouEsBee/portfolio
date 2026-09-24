e0e7b0855e987a88f513b227009db696ab8b1883
Teo Wen Kai <wenkai04@hotmail.com>
2026-09-24 14:28:15 +0800

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ProjectsPage from './pages/ProjectsPage'
 import IndividualProjectPage from './pages/IndividualProjectPage'
-import Layout from './layout'
+import Layout from './Layout'
 import NotFoundPage from './pages/NotFoundPage'
 import ResumePage from './pages/ResumePage'
 
